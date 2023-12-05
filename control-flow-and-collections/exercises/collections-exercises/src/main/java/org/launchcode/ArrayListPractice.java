6e53c981165465dd3cb88f5de805a2bf1f2b2658
ngarner98 <exmachina091098@gmail.com>
2023-12-04 20:19:25 -0600

    exercise

---
+++ control-flow-and-collections/exercises/collections-exercises/src/main/java/org/launchcode/ArrayListPractice.java
@@ -0,0 +1,12 @@
+package org.launchcode;
+//public class ArrayListPractice {
+//    public static int sumEven(ArrayList<Integer> arr) {
+//        int total = 0;
+//        for (int integer : arr) {
+//            if (integer % 2 == 0) {
+//                total += integer;
+//            }
+//        }
+//        return total;
+//    }
+//}
---
+++ control-flow-and-collections/exercises/collections-exercises/src/main/java/org/launchcode/ArrayPractice.java
@@ -0,0 +1,14 @@
+package org.launchcode;
+import java.util.Arrays;
+
+public class ArrayPractice {
+    public static void main(String[] args){
+        int[] integerArray = {1, 1, 2, 3, 5, 8};
+        for (int i = 0; i < integerArray.length; i++) {
+            System.out.println(integerArray[i]);
+        }
+        String quote = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+        String[] quoteArray = quote.split("\\.");
+        System.out.println(Arrays.toString(quoteArray));
+    }
+}
